9700bf67bd41ba2e490d5e1776890e95e102a966
AntoineDlb <antoine.delebecque@supinfo.com>
2025-11-03 12:50:39 +0100

    Replace Renderer with UI_Cli and UI_Qt views
    Removed the generic Renderer view and introduced separate UI_Cli and UI_Qt views for CLI and Qt interfaces. Updated CMakeLists.txt to include the new view files and remove references to Renderer.

---
+++ include/views/UI_Cli.h
@@ -0,0 +1,13 @@
+//
+// Created by antoi on 03/11/2025.
+//
+
+#ifndef LAYING_GRASS_PROJECT_UI_CLI_H
+#define LAYING_GRASS_PROJECT_UI_CLI_H
+
+namespace Views {
+    class UI_Cli {
+    };
+} // Views
+
+#endif //LAYING_GRASS_PROJECT_UI_CLI_H
---
+++ src/views/UI_Cli.cpp
@@ -0,0 +1,8 @@
+//
+// Created by antoi on 03/11/2025.
+//
+
+#include "../../include/views/UI_Cli.h"
+
+namespace Views {
+} // Views

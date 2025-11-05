12e1ea0d1091f190cbc2cbced50d391522dfe181
Louis-Miroir <louis.miroir@gmail.com>
2025-11-05 20:40:18 +0100

    Class Player sans les notions de Board
    pas encore accès à la class board

--- include/models/Player.h
+++ include/models/Player.h
@@ -4,10 +4,41 @@
 
 #ifndef LAYING_GRASS_PROJECT_PLAYER_H
 #define LAYING_GRASS_PROJECT_PLAYER_H
+#include <string>
 
 namespace Models {
+    class Board;
     class Player {
+    private :
+        int id;
+        std::string name;
+        int score=0;
+        int exchange = 0;
+        std::string color;
+
+
+    public:
+        Player(int id,std::string name, std::string color);
+
+        int getId() const noexcept { return id; }
+        void setId(int newId) noexcept { id = newId; }
+
+        const std::string &getName() const noexcept { return name; }
+        void setName(const std::string &newName) { name = newName; }
+
+        int getScore() const noexcept { return score; }
+        void setScore(int newScore) noexcept { score = newScore; }
+
+        int getExchange() const noexcept { return exchange; }
+        void setExchange(int newExchange) noexcept { exchange = newExchange; }
+
+        const std::string &getColor() const noexcept { return color; }
+        void setColor(const std::string &newColor) { color = newColor; }
+
+        int calculateScore(const Board& board) const;
+        int calculateCellsPlaced(const Board& board) const;
+
+        //destruvter !!!!
     };
 } // Models
-
 #endif //LAYING_GRASS_PROJECT_PLAYER_H
--- src/models/Player.cpp
+++ src/models/Player.cpp
@@ -3,6 +3,21 @@
 //
 
 #include "../../include/models/Player.h"
+#include "../../include/models/Board.h"
+#include <vector>
 
 namespace Models {
+    Player::Player(int newId, std::string name, std::string color) {
+        id = newId;
+    }
+
+    int Player::calculateScore() {
+        return score;
+    }
+
+    int Player::calculateCellsPlaced() {
+
+        return 0;
+    }
+
 } // Models
